07f86bd2a97728eaa3db0811bfa675ecea5b18d5
hanzheng <kewangxunqiu@163.com>
2020-03-24 11:42:17 +0800

    update

--- src/data/baobiandianqi.js
+++ src/data/baobiandianqi.js
@@ -1,23 +1,36 @@
 export default {
-    title: "保变电气",
-    currentPrice: 6,
-    step: 0.01,
-    currentNum: null,
-    actualPrice: null,
-    remainPrice: null,
-    nextDayPercent: null,
-    allPercent: [],
-    detail: [{
-        time: "3月25日",
-        end: 40.83, // 收盘价
-        buyAndSale: [{
-                buyPrice: 6, // 买入价
-                buyNum: 3200 // 买入数量
-            },
-            {
-                buyPrice: 6.24, // 买入价
-                buyNum: 1500 // 买入数量
-            }
-        ]
-    }]
-}
+  title: "保变电气",
+  currentPrice: 6,
+  step: 0.01,
+  currentNum: null,
+  actualPrice: null,
+  remainPrice: null,
+  nextDayPercent: null,
+  allPercent: [],
+  detail: [
+    {
+      time: "3月22日",
+      end: 40.83, // 收盘价
+      buyAndSale: [
+        {
+          buyPrice: 6, // 买入价
+          buyNum: 3200 // 买入数量
+        },
+        {
+          buyPrice: 6.24, // 买入价
+          buyNum: 1500 // 买入数量
+        }
+      ]
+    },
+    {
+      time: "3月23日",
+      end: 40.83, // 收盘价
+      buyAndSale: [
+        {
+          buyPrice: 6, // 买入价
+          buyNum: 3200 // 买入数量
+        }
+      ]
+    }
+  ]
+};
--- src/data/zhongxing.js
+++ src/data/zhongxing.js
@@ -1,18 +1,36 @@
 export default {
-    title: "中兴通讯",
-    currentPrice: 40.83,
-    step: 0.1,
-    currentNum: null,
-    actualPrice: null,
-    remainPrice: null,
-    nextDayPercent: null,
-    allPercent: [],
-    detail: [{
-        time: "3月25日",
-        end: 40.83, // 收盘价
-        buyAndSale: [{
-            buyPrice: 40.46, // 买入价
-            buyNum: 200 // 买入数量
-        }]
-    }]
-}
+  title: "中兴通讯",
+  currentPrice: 40.83,
+  step: 0.1,
+  currentNum: null,
+  actualPrice: null,
+  remainPrice: null,
+  nextDayPercent: null,
+  allPercent: [],
+  detail: [
+    {
+      time: "3月23日",
+      end: 40.83, // 收盘价
+      buyAndSale: [
+        {
+          buyPrice: 40.46, // 买入价
+          buyNum: 200 // 买入数量
+        }
+      ]
+    },
+    {
+      time: "3月24日",
+      end: 40.83, // 收盘价
+      buyAndSale: [
+        {
+          buyPrice: 42.09, // 买入价
+          buyNum: -200 // 买入数量
+        },
+        {
+          buyPrice: 41.61, // 买入价
+          buyNum: 300 // 买入数量
+        }
+      ]
+    }
+  ]
+};
